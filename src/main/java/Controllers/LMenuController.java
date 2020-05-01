59a5e9e24b76384254095b6c1ad40f4a7dcf24a8
Ciuban Casian <ciubancasian@yahoo.com>
2020-05-01 18:35:34 +0300

    Created the GUI for the add/remove button window and link it to the add/remove button

--- src/main/java/Controllers/LMenuController.java
+++ src/main/java/Controllers/LMenuController.java
@@ -1,16 +1,30 @@
 package Controllers;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.awt.*;
+import java.io.IOException;
 
-public class LMenuController extends Application {
+public class LMenuController {
+    @FXML
+    public Button addremove;
+    @FXML
+    public void AddRemoveButtonAction() {
+        try {
+            Stage stage = (Stage) addremove.getScene().getWindow();
+            Parent AddRemoveMenu = FXMLLoader.load(getClass().getClassLoader().getResource("Add_Remove_Books_menu.fxml"));
+            Scene scene = new Scene(AddRemoveMenu, 750, 300);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    public static void main(String[] args) {
-        launch(args);
+        return;
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }

1fcb7bdc56a3fdf315dfab81d1fa80fcffce294f
Ciuban Casian <ciubancasian@yahoo.com>
2020-05-18 21:11:53 +0300

    Added tests for the add book method

--- src/main/java/Controllers/AddRemoveBooksMenu.java
+++ src/main/java/Controllers/AddRemoveBooksMenu.java
@@ -26,6 +26,72 @@ public class AddRemoveBooksMenu  {
     @FXML
     private Text message;
 
+    public void AddInFile(String title, String author, String publishinghouse, String date, String category, String path){
+
+// Citesc din fisier
+
+        JSONArray list = new JSONArray();
+        JSONParser parser = new JSONParser();
+        try( Reader reader = new FileReader(path)){
+
+            JSONArray jsonArray = (JSONArray) parser.parse(reader);
+            //System.out.println(jsonArray);
+
+
+            Iterator<Object> it = jsonArray.iterator();
+            while (it.hasNext()) {
+                JSONObject obj = (JSONObject) it.next();
+                //System.out.println(obj.get("title") + " - " + obj.get("author") + " - " + obj.get("amount"));
+                list.add(obj);
+
+                //System.out.println(list.toString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+// Adaug daca trebuie sau incrementez
+
+        int am_gasit=0;
+        Iterator<Object> it = list.iterator();
+        while (it.hasNext()) {
+            JSONObject obj = (JSONObject) it.next();
+            //System.out.println(obj);
+            if ((am_gasit == 0) && (title.equals(obj.get("title")))
+                    && (author.equals(obj.get("author")))
+                    && (publishinghouse.equals(obj.get("publishinghouse")))
+                    && (date.equals(obj.get("date")))
+                    && (category.equals(obj.get("category")))) {
+                obj.replace("amount", ((Long) obj.get("amount") + 1));
+                am_gasit = 1;
+                //System.out.println("1");
+
+            }
+        }
+        if(am_gasit != 1)
+        {
+            JSONObject obj1 = new JSONObject();
+            obj1.put("title", title);
+            obj1.put("author", author);
+            obj1.put("publishinghouse", publishinghouse);
+            obj1.put("date", date);
+            obj1.put("category", category);
+            obj1.put("amount", 1);
+
+            list.add(obj1);
+            //System.out.println("2");
+        }
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(list.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     public void AddBookAction() throws FileNotFoundException {
 
@@ -59,67 +125,7 @@ public class AddRemoveBooksMenu  {
             return;
         }
 
-// Citesc din fisier
+        AddInFile(titleField.getText(),authorField.getText(),publishinghouseField.getText(),dateField.getText(),(String) selectcategory.getValue(),"src\\main\\resources\\LibraryBooks.json");
 
-        JSONArray list = new JSONArray();
-        JSONParser parser = new JSONParser();
-        try( Reader reader = new FileReader("src\\main\\resources\\LibraryBooks.json")){
-
-            JSONArray jsonArray = (JSONArray) parser.parse(reader);
-            //System.out.println(jsonArray);
-
-
-            Iterator<Object> it = jsonArray.iterator();
-            while (it.hasNext()) {
-                JSONObject obj = (JSONObject) it.next();
-                //System.out.println(obj.get("title") + " - " + obj.get("author") + " - " + obj.get("amount"));
-                list.add(obj);
-
-                //System.out.println(list.toString());
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-
-// Adaug daca trebuie sau incrementez
-
-        int am_gasit=0;
-        Iterator<Object> it = list.iterator();
-        while (it.hasNext()) {
-            JSONObject obj = (JSONObject) it.next();
-            //System.out.println(obj);
-            if ((am_gasit == 0) && (titleField.getText().equals(obj.get("title")))
-                    && (authorField.getText().equals(obj.get("author")))
-                    && (publishinghouseField.getText().equals(obj.get("publishinghouse")))
-                    && (dateField.getText().equals(obj.get("date")))
-                    && (selectcategory.getValue().equals(obj.get("category")))) {
-                obj.replace("amount", ((Long) obj.get("amount") + 1));
-                am_gasit = 1;
-                //System.out.println("1");
-
-            }
-        }
-        if(am_gasit != 1)
-            {
-                JSONObject obj1 = new JSONObject();
-                obj1.put("title", titleField.getText());
-                obj1.put("author", authorField.getText());
-                obj1.put("publishinghouse", publishinghouseField.getText());
-                obj1.put("date", dateField.getText());
-                obj1.put("category", selectcategory.getValue());
-                obj1.put("amount", 1);
-
-                list.add(obj1);
-                //System.out.println("2");
-            }
-        try (FileWriter file = new FileWriter("src\\main\\resources\\LibraryBooks.json")) {
-            file.write(list.toJSONString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
---
+++ src/main/test/Controllers/AddRemoveBooksMenuTest.java
@@ -0,0 +1,114 @@
+package Controllers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class AddRemoveBooksMenuTest {
+
+    AddRemoveBooksMenu ARBM;
+    @Before
+    public void setUp() throws Exception {
+        ARBM = new AddRemoveBooksMenu();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ARBM = null;
+    }
+
+    @Test
+    public void testAddNewBook() {
+
+
+        JSONArray list = new JSONArray();
+        JSONParser parser = new JSONParser();
+        try( Reader reader = new FileReader("src\\main\\test\\resources\\LibraryBooks.json")){
+
+            JSONArray jsonArray = (JSONArray) parser.parse(reader);
+            //System.out.println(jsonArray);
+
+
+            Iterator<Object> it = jsonArray.iterator();
+            while (it.hasNext()) {
+                JSONObject obj = (JSONObject) it.next();
+                //System.out.println(obj.get("title") + " - " + obj.get("author") + " - " + obj.get("amount"));
+                list.add(obj);
+
+                //System.out.println(list.toString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String title="AAA"; String author="BBB"; String publishinghouse="CCC"; String date="DDD"; String category="EEE";
+        ARBM.AddInFile("AAA","BBB","CCC","DDD","EEE","src\\main\\test\\resources\\LibraryBooks.json");
+
+        JSONArray list1 = new JSONArray();
+        JSONParser parser1 = new JSONParser();
+        try( Reader reader1 = new FileReader("src\\main\\test\\resources\\LibraryBooks.json")){
+
+            JSONArray jsonArray1 = (JSONArray) parser1.parse(reader1);
+            //System.out.println(jsonArray);
+
+
+            Iterator<Object> it1 = jsonArray1.iterator();
+            while (it1.hasNext()) {
+                JSONObject obj1 = (JSONObject) it1.next();
+                //System.out.println(obj.get("title") + " - " + obj.get("author") + " - " + obj.get("amount"));
+                list1.add(obj1);
+
+                //System.out.println(list.toString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        int am_gasit=0;
+        Iterator<Object> it = list1.iterator();
+        while (it.hasNext()) {
+            JSONObject obj = (JSONObject) it.next();
+            //System.out.println(obj);
+            if ((am_gasit == 0) && (title.equals(obj.get("title")))
+                    && (author.equals(obj.get("author")))
+                    && (publishinghouse.equals(obj.get("publishinghouse")))
+                    && (date.equals(obj.get("date")))
+                    && (category.equals(obj.get("category")))) {
+                //System.out.println(11);
+                am_gasit = 1;
+            }
+        }
+
+        //System.out.println(list);System.out.println(list1);
+        try (FileWriter file = new FileWriter("src\\main\\test\\resources\\LibraryBooks.json")) {
+            file.write(list.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(am_gasit);
+        Assert.assertTrue(am_gasit==1);
+
+    }
+}

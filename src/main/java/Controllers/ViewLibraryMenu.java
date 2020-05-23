9ec699ee56da4271e632797e89eeb558369f2260
Ciuban Casian <ciubancasian@yahoo.com>
2020-05-23 19:18:43 +0300

    Added functionality to the Borrow buttons in the displayed list of books.

--- src/main/java/Controllers/ViewLibraryMenu.java
+++ src/main/java/Controllers/ViewLibraryMenu.java
@@ -4,12 +4,15 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -20,23 +23,28 @@ import org.json.simple.parser.ParseException;
 
 import java.awt.*;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Iterator;
-import java.util.Observable;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static Controllers.LoginController.UsernameGlobal;
 
 public class ViewLibraryMenu {
 
     @FXML
-    public Button Back;
+    private Button Back;
     @FXML
-    public TextField SearchBar;
+    private TextField SearchBar;
     @FXML
-    public ListView Display;
+    private ListView Display;
     @FXML
-    public Button Search;
+    private Button Search;
     @FXML
-    public ComboBox Filter;
+    private ComboBox Filter;
+    @FXML
+    private Label Message;
 
     @FXML
     public void Back() {
@@ -94,6 +102,115 @@ public class ViewLibraryMenu {
         return list;
     }
 
+    public int BorrowBook(String title, String author, String path1, String path2){
+
+        // Un user poate imprumuta doar un singur exemplar din fiecare carte
+
+        String username = UsernameGlobal;
+
+        // Deschid si citesc din fisierul LibraryBooks
+
+        JSONObject cartea = new JSONObject();
+
+        JSONArray list = new JSONArray();
+        JSONParser parser = new JSONParser();
+        try( Reader reader = new FileReader(path1)){
+
+            JSONArray jsonArray = (JSONArray) parser.parse(reader);
+
+            Iterator<Object> it = jsonArray.iterator();
+            while (it.hasNext()) {
+                JSONObject obj = (JSONObject) it.next();
+                if(obj.get("title").toString().equals(title) && obj.get("author").toString().equals(author)){
+                    String time = new SimpleDateFormat("yyyy MM dd HH mm ss").format(Calendar.getInstance().getTime());
+                    cartea.put("title",obj.get("title").toString());
+                    cartea.put("author",obj.get("author").toString());
+                    cartea.put("date",obj.get("date").toString());
+                    cartea.put("publishinghouse",obj.get("publishinghouse").toString());
+                    cartea.put("category",obj.get("category").toString());
+                    cartea.put("time",time);
+                }
+                list.add(obj);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // Deschid fisierul UserBooks si caut sa vad daca exista cartea
+        JSONArray list1 = new JSONArray();
+        JSONParser parser1 = new JSONParser();
+        try( Reader reader1 = new FileReader(path2)){
+
+            JSONArray jsonArray1 = (JSONArray) parser1.parse(reader1);
+
+            Iterator<Object> it1 = jsonArray1.iterator();
+            while (it1.hasNext()) {
+                JSONObject obj1 = (JSONObject) it1.next();
+                list1.add(obj1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // Verific sa nu aiba deja cartea
+        Iterator<Object> it2 = list1.iterator();
+        while (it2.hasNext()) {
+            JSONObject obj2 = (JSONObject) it2.next();
+            if(obj2.get("username").toString().equals(username)){
+                if(obj2.get("books") != null){
+                    JSONArray caut = (JSONArray) obj2.get("books");
+                    Iterator<Object> i = caut.iterator();
+                    while (i.hasNext()){
+                        JSONObject o = (JSONObject) i.next();
+                        if(o.get("title").toString().equals(title)){
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Daca nu mai exista cartea atunci decrementez cu 1 cartea din LibraryBooks
+        Iterator<Object> it1 = list.iterator();
+        while (it1.hasNext()) {
+            JSONObject obj = (JSONObject) it1.next();
+            if(obj.get("title").toString().equals(title) && obj.get("author").toString().equals(author)){
+                obj.replace("amount",obj.get("amount").hashCode()-1);
+            }
+        }
+
+        // Scriu cartea in UserBooks si rescriu LibraryBooks
+
+        Iterator<Object> it3 = list1.iterator();
+        while (it3.hasNext()) {
+            JSONObject obj2 = (JSONObject) it3.next();
+            if(obj2.get("username").toString().equals(username)){
+
+                JSONArray caut = (JSONArray) obj2.get("books");
+                caut.add(cartea);
+            }
+        }
+
+        try (FileWriter file = new FileWriter(path1)) {
+            file.write(list.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter file = new FileWriter(path2)) {
+            file.write(list1.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return 1;
+    }
+
     @FXML
     public void Search(){
 
@@ -106,8 +223,22 @@ public class ViewLibraryMenu {
         Iterator<Object> it = list.iterator();
         while (it.hasNext()) {
             JSONObject obj = (JSONObject) it.next();
+            Button borrow = new Button("Borrow");
             String aux = obj.get("title").toString()+" - "+obj.get("author").toString();
-            afis.add(aux);
+            afis.addAll(aux,borrow);
+            borrow.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    String title = obj.get("title").toString();
+                    String author = obj.get("author").toString();
+                    if(BorrowBook(title,author,"src\\main\\resources\\LibraryBooks.json","src\\main\\resources\\UsersBooks.json") == 0){
+                        Message.setText("You already borrowed this book!");
+                    }
+                    else{
+                        Message.setText("");
+                    }
+                }
+            });
         }
 
     }

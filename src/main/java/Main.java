4c81099a4d068f24ef320b3fb57a99abbd9e172a
Bogdan Colhon <nbat99@yahoo.com>
2020-05-25 21:47:43 +0300

    Changed the files path for the jar executable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
+import Services.FileSystemService;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
 
 public class Main extends Application{
     public static void main(String [] args)
     {
         launch(args);
+
     }
     public void start(Stage primaryStage) throws Exception
     {
-
+        FileUtils.copyURLToFile(Main.class.getClassLoader().getResource("users.json"), FileSystemService.getPathToFile("classes", "users.json").toFile());
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Library Administration");
         primaryStage.setScene(new Scene(root, 300, 275));

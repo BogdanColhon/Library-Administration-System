d22a01b08b343dd3c190de15f55b1b457f96852e
Bogdan Colhon <nbat99@yahoo.com>
2020-06-13 13:34:59 +0300

    Linked the ListView with UsersBooks.json

--- src/main/java/Controllers/ViewUsers.java
+++ src/main/java/Controllers/ViewUsers.java
@@ -27,6 +27,30 @@ import java.util.concurrent.TimeUnit;
 public class ViewUsers {
     @FXML
     public Button back;
+    @FXML
+    private ListView display;
+
+    public long calculateRemainTime(String borrowed_date) {
+
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy MM dd HH mm ss");
+
+        java.util.Date borrowedDate;
+        Calendar current = Calendar.getInstance();
+        java.util.Date currentDate;
+        String current_date = format.format(current.getTime());
+        try {
+            borrowedDate = format.parse(borrowed_date);
+            currentDate = format.parse(current_date);
+            long diffInMillies = currentDate.getTime() - borrowedDate.getTime();
+            long diffence_in_seconds = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+            // 14 Days in seconds: 1209600
+            return 1209600 - diffence_in_seconds;
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
     public void Back() {
         try {
@@ -39,8 +63,49 @@ public class ViewUsers {
         }
     }
 
+    public void ListUsers() throws IOException {
+        JSONArray list = new JSONArray();
+        JSONParser parser1 = new JSONParser();
+        try (Reader reader1 = new FileReader("src\\main\\resources\\UsersBooks.json")) {
+
+            JSONArray jsonArray1 = (JSONArray) parser1.parse(reader1);
+
+            Iterator<Object> it1 = jsonArray1.iterator();
+            while (it1.hasNext()) {
+                JSONObject obj1 = (JSONObject) it1.next();
+                list.add(obj1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ObservableList afis = display.getItems();
+        afis.clear();
+        Iterator<Object> it2 = list.iterator();
+        while (it2.hasNext()) {
+            JSONObject obj2 = (JSONObject) it2.next();
+
+            String aux = obj2.get("username").toString();
+            JSONArray list1 = (JSONArray) obj2.get("books");
+            Iterator<Object> it3 = list1.iterator();
+            while (it3.hasNext()) {
+                JSONObject obj = (JSONObject) it3.next();
+                String aux1 = aux + " - " + obj.get("title").toString() + " - " + obj.get("author").toString();
+                long x = calculateRemainTime(obj.get("time").toString());
+                int days = (int) (x / 86400);
+                int hours = (int) ((x - (days * 86400)) / 3600);
+                int min = (int) ((x - (days * 86400) - (hours * 3600)) / 60);
+                int sec = (int) (x - (days * 86400) - (hours * 3600) - (min * 60));
+                String aux2 = aux1 + " - " + "Remain time: " + days + " days ";
+                Button notify = new Button("Notify");
+                afis.addAll(aux2, notify);
+                
+
+            }
+
+        }
     }
-
-
-
+}
 
